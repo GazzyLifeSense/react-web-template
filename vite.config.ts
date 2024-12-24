6d0435aad88738566722793f73e64d56218921eb
lotusFoundAnotherWorld <2636258590@qq.com>
2024-12-24 15:26:41 +0800

    add vite-plugin-eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import { resolve } from "path"
+import react from "@vitejs/plugin-react"
+// @ts-expect-error unnecessary
+import eslint from "vite-plugin-eslint"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  root: resolve(__dirname, "src"),
+  plugins: [react(), eslint()]
 })
