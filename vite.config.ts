2ab2f96f45495a7c0da72e3e4f230ba277e16202
lotusFoundAnotherWorld <2636258590@qq.com>
2025-01-07 14:55:51 +0800

    add tailwindcss and autoprefixer

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite"
+import autoprefixer from "autoprefixer"
+import tailwindcss from "tailwindcss"
 import react from "@vitejs/plugin-react"
 // @ts-expect-error unnecessary
 import eslint from "vite-plugin-eslint"
 
 // https://vite.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer]
+    }
+  },
   plugins: [react(), eslint()]
 })
